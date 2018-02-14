15e4d3367590307ad9b6226206ca4ef47bceefb6
Azat Arslanov <a.r.arslanov@gmail.com>
2018-02-14 21:31:08 +0300

    add calculating column's neighbors

--- src/main/kotlin/org/numenta/htm/Field.kt
+++ src/main/kotlin/org/numenta/htm/Field.kt
@@ -2,15 +2,17 @@ package org.numenta.htm
 
 import java.util.*
 import kotlin.collections.ArrayList
+import kotlin.collections.HashMap
 
 class Field(size: Int) {
     private val matrix: MutableList<MutableList<Column>>
+    private val sideSize: Int = Math.sqrt(size.toDouble()).toInt()
+    private val cache: MutableMap<Pair<Column, Int>, List<Column>> = HashMap()
 
     val columns: List<Column>
 
 
     init {
-        val sideSize = Math.sqrt(size.toDouble()).toInt()
         matrix = ArrayList()
         val list = ArrayList<Column>()
         for (i in 0 until sideSize) {
@@ -33,6 +35,35 @@ class Field(size: Int) {
             matrix.add(rowToAdd)
         }
         columns = Collections.unmodifiableList(list)
+    }
+
+
+    fun calculateNeighbors(column: Column, radius: Int) : List<Column> {
+        val indexOf = columns.indexOf(column)
+        if (indexOf == -1) {
+            throw IllegalArgumentException("Column isn't in Field")
+        }
+        val key = Pair(column, radius)
+        if (cache.contains(key)) {
+            return cache[key] ?: ArrayList()
+        }
+        val rowNumber = indexOf / sideSize
+        val columnNumber = indexOf % sideSize
+        val firstRow = if (rowNumber - radius < 0) 0 else rowNumber - radius
+        val lastRow = if (rowNumber + radius >= matrix.size) matrix.size - 1 else rowNumber + radius
+        var result: MutableList<Column> = ArrayList()
+        for (i in firstRow..lastRow) {
+            val currentRow = matrix[i]
+            val firstColumn = if (columnNumber - radius < 0) 0 else columnNumber - radius
+            val lastColumn = if (columnNumber + radius >= currentRow.size) currentRow.size -1 else columnNumber + radius
+            for (j in firstColumn..lastColumn){
+                result.add(currentRow[j])
+            }
+        }
+        result.remove(column)
+        result = Collections.unmodifiableList(result)
+        cache[key] = result
+        return result
     }
 
 }
--- src/test/kotlin/org/numenta/htm/FieldTest.kt
+++ src/test/kotlin/org/numenta/htm/FieldTest.kt
@@ -7,8 +7,29 @@ class FieldTest {
 
     @Test
     fun initialization() {
-        val size = 17
+        val size = 0
         val field = Field(size)
         assertTrue(size == field.columns.size)
+    }
+
+    @Test
+    fun calculateNeighbors() {
+        var size = 25
+        var index = 0
+        var radius = 1
+        var field = Field(size)
+        var neighbors = field.calculateNeighbors(field.columns[index], radius)
+        assertTrue(neighbors.size == 3)
+
+        index = 24
+        neighbors = field.calculateNeighbors(field.columns[index], radius)
+        assertTrue(neighbors.size == 3)
+
+        size = 11
+        index = 10
+        radius = 2
+        field = Field(size)
+        neighbors = field.calculateNeighbors(field.columns[index], radius)
+        assertTrue(neighbors.size == 7)
     }
 }
