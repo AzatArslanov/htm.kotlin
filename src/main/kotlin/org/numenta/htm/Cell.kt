e29cfa2b49a3fc568aa934ff6a345058d246f037
Azat Arslanov <a.r.arslanov@gmail.com>
2018-03-02 19:35:58 +0300

    update Cell - add field to future update

--- src/main/kotlin/org/numenta/htm/Cell.kt
+++ src/main/kotlin/org/numenta/htm/Cell.kt
@@ -19,11 +19,13 @@ class Cell(isPredictive: Boolean = false, isActive:Boolean = false, isLearn:Bool
             field = value
         }
 
+    var toUpdate: Cell.Update? = null
+
     private var isPastPredictive: Boolean = false
     private var isPastActive: Boolean = false
     private var isPastLearn: Boolean = false
 
-    fun isActive(time: Time):Boolean = if (time == Time.NOW) isNowActive else isPastActive
+    fun isActive(time: Time): Boolean = if (time == Time.NOW) isNowActive else isPastActive
     fun isLearn(time: Time): Boolean = if (time == Time.NOW) isNowLearn else isPastLearn
     fun isPredictive(time: Time): Boolean = if (time == Time.NOW) isNowPredictive else isPastPredictive
 
@@ -34,5 +36,7 @@ class Cell(isPredictive: Boolean = false, isActive:Boolean = false, isLearn:Bool
 
 
     fun getBestMatchingSegment(time: Time, minThreshold: Int): Segment? = segments.filter { it.activity(time) >= minThreshold }.maxBy { it.activity(time) }
+
+    class Update(val sequenceSegment: Boolean, val segment: Segment, val synapses: List<InnerSynapse>)
 
 }
